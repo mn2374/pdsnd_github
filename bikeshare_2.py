5e1f131f99a530d2681c7e04bca075ab206ba867
michele.nguyen <michele.nguyen@ibm.com>
2020-05-31 22:45:23 -0700

    Corrected naming of proper nouns (cities)

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -6,7 +6,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
 
-CITIES = ["chicago", "new york city", "washington"]
+CITIES = ["Chicago", "New York City", "Washington"]
 MONTHS = ["all", "january", "febuary", "march", "april", "may", "june",
           "july", "august", "september", "october", "november", "december"]
 DAYS = ["all", "monday", "tuesday", "wednesday",
